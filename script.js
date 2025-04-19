0c46137a3fe3d8de203d9fc214d637aa407e1e73
Kanchan <priyadarshikanchan30052002@gmail.com>
2025-04-19 20:45:46 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("Hey this is Kanchan")
+let a = 5;
+let b = 6;
+let c = "Kanchan"
+// var 55="priya";      not allowed
+console.log(a+b)
+console.log(typeof a, typeof b, typeof c)
+
+// const a1=5;
+// a1=a1+1;            not allowed because it is constant it cannot change like this.
+{
+    let a=66;
+    console.log(a)
+
+}
+console.log(a)
+
+
+let o={
+    "name": "kanchan",
+    "job role": "softwer engineer"
+}
+console.log(o);
+o.salary="500 crores";
+console.log(o);
